46d13ebe86083a8c989f2c12f51a117e2c87e870
ZakiaTaou <zakiataou99@gmail.com>
2025-12-10 12:42:43 +0100

    creation et la connexion de la database

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("hello")
+import express from "express";
+import dotenv from "dotenv";
+import { connectionDb } from "./src/config/database.js";
+
+const app = express();
+
+app.use(express.json());
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
+  await connectionDb();
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
